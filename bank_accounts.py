51f2cf31f92cc80653a9616144c316531c3117f0
amandatorresapu <69279617+amandatorresapu@users.noreply.github.com>
2022-03-08 15:07:01 -0800

    updates

---
+++ bank_accounts.py
@@ -0,0 +1,33 @@
+class BankAccount:
+    def __init__(self, int_rate =.02, balance=200): 
+        self.int_rate = int_rate
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print(self.balance)
+        return self
+
+    def withdraw(self, amount):
+        self.balance -= amount
+        print(self.balance)
+        return self
+
+    def display_account_info(self):
+        print(f"Balance:$ {self.balance}")
+        return self
+
+    def yield_interest(self):
+        interest = (self.balance * self.int_rate)
+        self.balance += interest
+        return self
+
+amanda = BankAccount(.02, 500)
+teola = BankAccount(.03, 500)
+max = BankAccount(.02, 500)
+# teola = BankAccount(2, 500)
+
+amanda.deposit(50).deposit(50).deposit(50).withdraw(50).yield_interest().display_account_info()
+teola.deposit(50).deposit(50).deposit(50).withdraw(50).yield_interest().display_account_info()
+max.deposit(50).deposit(50).deposit(50).withdraw(50).yield_interest().display_account_info()
+
